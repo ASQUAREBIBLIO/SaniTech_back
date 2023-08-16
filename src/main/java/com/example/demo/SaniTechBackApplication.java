82f1e9874023341eda03fcb291cf675a45d62757
AHMED ACH-CHATIBI <102978631+ASQUAREBIBLIO@users.noreply.github.com>
2023-08-16 09:48:33 +0000

    Fait: git init

---
+++ src/main/java/com/example/demo/SaniTechBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaniTechBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaniTechBackApplication.class, args);
+	}
+
+}
